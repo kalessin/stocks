8c4af2d15e1f3ec96923020fd48d11c7ce788c27
Martin Olveyra <molveyra@gmail.com>
2018-09-28 22:51:58 -0300

    allow to scrape annual data

--- stocks/spiders/tagnifi.py
+++ stocks/spiders/tagnifi.py
@@ -2,11 +2,14 @@ from scrapy import Spider, Request
 from scrapy.exceptions import NotConfigured
 
 
+AVL_PERIODS = {'quarter', 'ttm', 'annual'}
+
+
 class TagnifiSpider(Spider):
     
     name = "tagnifi"
     BASE_URL = ('https://viewer.tagnifi.com/api/fundamentals?company={company}&statement={statement}'
-               '&period_type={period}&relative_period=0&limit={limit}&industry_template=commercial')
+               '&period_type={period_type}&relative_period=0&limit={limit}&industry_template=commercial')
     STATEMENTS = [('balance_sheet_statement', 'quarter'),
                   ('income_statement', 'ttm'),
                 ('cash_flow_statement', 'ttm')]
@@ -14,6 +17,7 @@ class TagnifiSpider(Spider):
     
     company = None
     limit = 1
+    period_type = None
 
     custom_settings = {
         "CONCURRENT_REQUEST": 1,
@@ -23,11 +27,14 @@ class TagnifiSpider(Spider):
     def start_requests(self):
         if self.company is None:
             raise NotConfigured
-        for statement, period in self.STATEMENTS:
-            yield Request(url=self.BASE_URL.format(company=self.company, statement=statement, period=period,
+        for statement, period_type in self.STATEMENTS:
+            period_type = self.period_type or period_type
+            yield Request(url=self.BASE_URL.format(company=self.company, statement=statement, period_type=period_type,
                                                    limit=self.limit),
-                          meta={'statement': statement})
+                          meta={'statement': statement, 'period_type': period_type, 'limit': self.limit})
     
     def parse(self, response):
         statement = response.meta['statement']
-        open(f"{self.company}-{statement}.json", "w").write(response.text)
+        period_type = response.meta['period_type']
+        limit = response.meta['limit']
+        open(f"{self.company}-{statement}-{period_type}-{limit}.json", "w").write(response.text)
